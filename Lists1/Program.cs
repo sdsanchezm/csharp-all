080018d8de1140dfdea0ea7782c6f8d1375596af
sdsanchezm <sdsanchezm@gmail.com>
2022-09-03 12:52:11 -0500

    working with lists

---
+++ Lists1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lists");
+
+
+            List<string> empanadaShopingList = new List<string>();
+
+            empanadaShopingList.Add("Beef Empanada");
+            empanadaShopingList.Add("Chicken Empanada");
+            empanadaShopingList.Add("Veggies Empanada");
+            empanadaShopingList.Add("4 apple soda");
+
+            for (int empanada = 0; empanada < empanadaShopingList.Count; empanada++)
+            {
+                Console.WriteLine(empanadaShopingList[empanada]);
+            }
+
+            empanadaShopingList.RemoveAt(3);
+
+            for (int empanada = 0; empanada < empanadaShopingList.Count; empanada++)
+            {
+                Console.WriteLine(empanadaShopingList[empanada]);
+            }
+
+        }
+    }
+}
