02a3420531d6373bfb8e06ba5c250b2f12f78cd0
sdsanchezm <sdsanchezm@gmail.com>
2022-09-02 20:50:36 -0500

    logical operators

---
+++ LogicOperators1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LogicOperators1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logic Operators");
+
+            bool value1 = true;
+            bool value2 = false;
+
+            // &&, ||, !
+
+            Console.WriteLine($"true && false is: {value1 && value2}");
+            Console.WriteLine(value1 || value2);
+            Console.WriteLine(value1 && !value2);
+            Console.WriteLine(value1 ^ value2);
+
+            // Different way to declare variable
+            var (xa, xb, xc) = (false, true, false);
+
+            // this is not permited because is implicitly changing the type pf a variable
+            // xa = 123;  
+
+            Console.WriteLine($"xa: {xa} xa: {xb} xa: {xc}");
+
+
+        }
+    }
+}
