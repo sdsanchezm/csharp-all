d6dbbb7de0297757328a4374d2eaa13ba02df6fb
sdsanchezm <sdsanchezm@gmail.com>
2022-09-03 10:57:52 -0500

    arrays documentation

---
+++ Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arrays in C sharp");
+
+            String[] carBrands;
+            float[] chessePrices;
+
+            carBrands = new string[] { "Lambo", "Ferrari", "Pontiac", "Honda"};
+            chessePrices = new float[] { 1.5f, 2.3f, 6.6f, 1.1f };
+
+            Console.WriteLine($"{carBrands.Length}");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"item {i} {carBrands[i]}: {chessePrices[i]}");
+            }
+
+        }
+    }
+}
