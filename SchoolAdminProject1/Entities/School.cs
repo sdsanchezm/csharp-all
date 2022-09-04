22a8ca6689d851eccedb04663fd83d12c23f3d4d
sdsanchezm <sdsanchezm@gmail.com>
2022-09-04 00:31:43 -0500

    School Project

---
+++ SchoolAdminProject1/Entities/School.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdminProject1.Entities
+{
+    class School
+    {
+        // this is the ATTRIBUTE name
+        string name;
+
+        // this is the PROPERTY that enables the getters and setters
+        public string Name
+        { 
+            get { return "Copy: " + name; }
+            set { name = value.ToUpper();  }
+        }
+
+        public int YearOfCreation { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        // this is the constructor: 
+        //public School(string name, int year)
+        //{
+        //    this.name = name;
+        //    this.YearOfCreation = year;
+        //}
+
+        // another way to write the constructor
+        // this is made by using 'igualacion por tuples' 
+        public School(string name, int year) => (Name, YearOfCreation) = (name, year);
+
+    }
+}
---
+++ SchoolAdminProject1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SchoolAdminProject1.Entities;
+
+namespace SchoolAdminProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var school = new School("Kraus School", 2021);
+            Console.WriteLine(school.Name);
+        }
+    }
+}
