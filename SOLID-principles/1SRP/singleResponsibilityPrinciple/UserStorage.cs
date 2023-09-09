6ce28384e47d1977016710480f2585c06e5d2313
sdsanchezm <sdsanchezm@gmail.com>
2023-09-08 22:17:00 -0500

    adding userStorage class to store users operations

---
+++ SOLID-principles/1SRP/singleResponsibilityPrinciple/UserStorage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+
+namespace singleResponsibilityPrinciple
+{
+    public class UserStorage<T>
+    {
+        private ObservableCollection<T> _users;
+        public UserStorage()
+        {
+            _users = new ObservableCollection<T>();
+        }
+
+        public T Add(T item)
+        {
+            _users.Add(item);
+            return item;
+        }
+
+        public T Remove (T item)
+        {
+            _users.Remove(item);
+            return item;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _users;
+        }
+    }
+}
