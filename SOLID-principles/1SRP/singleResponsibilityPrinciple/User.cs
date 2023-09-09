8a151e25700f39c402e223ccd6ef6d7ca8d7f62a
sdsanchezm <sdsanchezm@gmail.com>
2023-09-08 22:25:21 -0500

    adding User entity

---
+++ SOLID-principles/1SRP/singleResponsibilityPrinciple/User.cs
@@ -0,0 +1,24 @@
+﻿namespace singleResponsibilityPrinciple
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public List<double> UserCodes { get; set; }
+
+        public User()
+        {
+            Name = string.Empty;
+            UserCodes = new List<double>();
+
+        }
+        public User(int id, string username, List<double> usercodes)
+        {
+            Id = id;
+            Username = username;
+            UserCodes = usercodes;
+        }
+    }
+}
