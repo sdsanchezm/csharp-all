a6771e8937d5160172e127d7f115376c62e24253
sdsanchezm <sdsanchezm@gmail.com>
2023-09-09 07:32:59 -0500

    adding another Generic option applying SRP - ExportCSV2

---
+++ SOLID-principles/1SRP/singleResponsibilityPrinciple/ExportHelper2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Reflection;
+using System.Text;
+
+namespace singleResponsibilityPrinciple
+{
+    public class ExportHelper2<T>
+    {
+        public void ExportToCSV2(IEnumerable<T> items)
+        {
+            var stringBuilder = new StringBuilder();
+            var header = GetTitleProperties();
+            stringBuilder.AppendLine(header);
+
+            foreach (var item in items)
+            {
+                var dataLine = GetLineCSV(item);
+                stringBuilder.AppendLine(dataLine);
+            }
+
+            var pathOfFile = SetPathOfFile();
+            File.WriteAllText(pathOfFile, stringBuilder.ToString(), Encoding.Unicode);
+        }
+
+        private string GetTitleProperties()
+        {
+            var properties = typeof(T).GetProperties();
+            var header = string.Join(";", properties.Select(prop => prop.Name));
+            return header;
+        }
+
+        private string GetLineCSV(T item)
+        {
+            var properties = typeof(T).GetProperties();
+            var dataRow = string.Join(";", properties.Select(prop => GetPropertyValue(prop, item)));
+            return dataRow;
+        }
+
+        private string GetPropertyValue(PropertyInfo prop, T item)
+        {
+            var propValue = prop.GetValue(item);
+            var propType = prop.PropertyType;
+
+            if (propType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(propType))
+            {
+                var enumerable = (IEnumerable)propValue;
+                var values = enumerable.Cast<object>().Select(x => x.ToString());
+                return string.Join("|", values);
+            }
+
+            return propValue?.ToString();
+        }
+
+        private string SetPathOfFile()
+        {
+            var typeOfData = typeof(T).Name;
+            var nameOfFile = $"File.{typeOfData}.csv";
+            var pathOfFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, nameOfFile);
+            return pathOfFile;
+        }
+
+    }
+}
--- SOLID-principles/1SRP/singleResponsibilityPrinciple/Program.cs
+++ SOLID-principles/1SRP/singleResponsibilityPrinciple/Program.cs
@@ -5,10 +5,19 @@
         public static void Main(string[] args)
         {
             UserRepo userRepository = new();
+            var userRepository2 = new UserRepo();
             //userRepo.Export();  
             //ExportHelper.ExportCSV(userRepository.GetAllUsers());
 
-            ExportCSVGeneric1<UserRepo>.Export(userRepository.GetAllUsers());
+            //ExportCSVGeneric1<UserRepo>.Export(userRepository.GetAllUsers());
+
+            //var export2 = new ExportHelper2<UserRepo>();
+            ExportHelper2<User> userExport = new();
+            userExport.ExportToCSV2(userRepository.GetAllUsers());
+            //export2.ExportToCSV2(userRepository.GetAllUsers());
+
+            Console.WriteLine(typeof(UserRepo));
+            //Console.WriteLine(typeof(userRepository2));
 
             Console.WriteLine("Process completed...");
         }
