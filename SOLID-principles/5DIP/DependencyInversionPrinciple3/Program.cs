cf734fdbdc3956c16318413202343f2ea2b7ba62
sdsanchezm <sdsanchezm@gmail.com>
2023-09-11 22:43:57 -0500

    adding 5th principle Dependency Inversion Principle examples 2

--- SOLID-principles/5DIP/DependencyInversionPrinciple3/Program.cs
+++ SOLID-principles/5DIP/DependencyInversionPrinciple3/Program.cs
@@ -1,3 +1,7 @@
+using DependencyInversionPrinciple3;
+using DependencyInversionPrinciple3.Models;
+using DependencyInversionPrinciple3.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DI here
+builder.Services.AddScoped<IUserRepo, UserRepo>();
+builder.Services.AddScoped<ILogbook, Logbook>();
 
 var app = builder.Build();
 
